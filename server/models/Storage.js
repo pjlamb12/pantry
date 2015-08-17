cae41b9dea6e5269680ea80c279c1f37ca7ccf52
Preston Lamb <plamb@axis41.com>
2015-08-17 14:16:03 -0600

    set up the Storage schema

--- server/models/Storage.js
+++ server/models/Storage.js
@@ -2,15 +2,24 @@ var mongoose = require('mongoose');
 
 var storageSchema = mongoose.Schema({
 	name: {type: String, required: '{PATH} is required!'},
-	contents: [String]
+	contents: { type: Array, "default": []}
 });
 
 var Storage = mongoose.model('Storage', storageSchema);
 
 function createDefaultStorageLocations () {
+	var item = {
+		"name": "Rice",
+		"quantity": 5,
+		"exp_date": "N/A",
+		"notes": "Testing"
+	};
+	var contents = new Array();
+	contents.push(item);
+	contents.push(item);
 	Storage.find({}).exec(function(err, collection){
 		if( collection.length === 0) {
-			Storage.create({name: 'Pantry', contents: []});
+			Storage.create({name: 'Pantry', contents: contents});
 		}
 	});
 };
